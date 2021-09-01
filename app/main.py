457dfecfd2dd9a4664c253cd4d5b5e6c8294c50f
baroodb <diabyoumar22@yahoo.fr>
2021-09-01 13:07:50 +0300

    Again

--- app/main.py
+++ app/main.py
@@ -5,5 +5,5 @@ app = FastAPI()
 
 @app.get('/')
 def read_root():
-    return {'Response': 'Hello and Welcome here to discuss with us today, we gonna have a lot of fun.'}
+    return {'Response': 'Hello and Welcome here to Madrid to discuss with us today, we gonna have a lot of fun.'}
 
