029cc2c209a0b1be4d8be56dcc98d7fd77ed9d88
baroodb <diabyoumar22@yahoo.fr>
2021-09-01 12:59:09 +0300

    Some changes on main.py

--- app/main.py
+++ app/main.py
@@ -5,5 +5,5 @@ app = FastAPI()
 
 @app.get('/')
 def read_root():
-    return {'Response': 'Hello and Welcome here to discuss with us today.'}
+    return {'Response': 'Hello and Welcome here to discuss with us today, we gonna have a lot of fun.'}
 
